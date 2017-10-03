f5c87d3977dd619c0058b1a47767cd511b714161
Muhammad-Almousa <30756099+Muhammad-Almousa@users.noreply.github.com>
2017-10-03 18:54:40 +0200

    Update media.py

--- media.py
+++ media.py
@@ -3,14 +3,15 @@ import webbrowser
 
 class Movie():
     """ This class provides a way to store movies related information"""
-    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
+        """This docstring explains the constructer method"""
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
         def show_trailer(self):
+            """This is a function to show the trailer of the movie"""
             webbrowser.open(self.trailer_youtube_url)
